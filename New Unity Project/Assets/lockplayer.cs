2dd65b1a368176069d213e940d516d3f6a7bf087
Orion Jakobovic <orionjak@gmail.com>
2018-09-16 19:06:39 -0400

    lock camera, ui starting

---
+++ New Unity Project/Assets/lockplayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ORKFramework;
+using ORKFramework.Behaviours;
+using Invector.vCharacterController;
+
+public class lockplayer : MonoBehaviour {
+    public vThirdPersonInput glorinput;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (ORK.Control.InEvent || ORK.Control.InMenu || ORK.Control.Blocked)
+        {
+            glorinput.lockInput = true;
+        }
+        else
+        {
+            glorinput.lockInput = false;
+        }  
+	}
+}
