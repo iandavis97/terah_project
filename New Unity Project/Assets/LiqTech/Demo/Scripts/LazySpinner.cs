16d53c74f300428f28e227d14f14354539778896
Orion Jakobovic <orionjak@gmail.com>
2018-10-13 18:04:23 -0400

    added a lot of stuff whew lad

---
+++ New Unity Project/Assets/LiqTech/Demo/Scripts/LazySpinner.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+namespace LiqTech
+{
+    public class LazySpinner : MonoBehaviour
+    {
+        public float Timescale = 1;
+
+        public bool SymmetricalDirectionSpeed;
+        public float MinAxisDirectionSpeed = -90;
+        public float MaxAxisDirectionSpeed = 90;
+        public float AxisDirectionAcceleration = 5;
+
+        public bool SymmetricalAxisSpeed;
+        public float MinAxisSpeed = 0;
+        public float MaxAxisSpeed = 180;
+        public float AxisAcceleration = 10;
+
+        public bool SymmetricalTurnSpeed;
+        public float MinTurnSpeed = 0;
+        public float MaxTurnSpeed = 360;
+        public float TurnAcceleration = 20;
+
+        private float axisDirectionPosition;
+        private float axisDirectionSpeed;
+        private Vector3 axis;
+        private float axisSpeed;
+        private float turnSpeed;
+
+        void OnValidate()
+        {
+            MinAxisDirectionSpeed = Mathf.Clamp(MinAxisDirectionSpeed, -MaxAxisDirectionSpeed, MaxAxisDirectionSpeed);
+            MinAxisSpeed = Mathf.Clamp(MinAxisSpeed, -MaxAxisSpeed, MaxAxisSpeed);
+            MinTurnSpeed = Mathf.Clamp(MinTurnSpeed, -MaxTurnSpeed, MaxTurnSpeed);
+
+            if (SymmetricalDirectionSpeed && MaxAxisDirectionSpeed < 0) MaxAxisDirectionSpeed = 0;
+            if (SymmetricalDirectionSpeed) MinAxisDirectionSpeed = -MaxAxisDirectionSpeed;
+            if (SymmetricalAxisSpeed && MaxAxisSpeed < 0) MaxAxisSpeed = 0;
+            if (SymmetricalAxisSpeed) MinAxisSpeed = -MaxAxisSpeed;
+            if (SymmetricalTurnSpeed && MinTurnSpeed < 0) MinTurnSpeed = 0;
+            if (SymmetricalTurnSpeed) MinTurnSpeed = -MaxTurnSpeed;
+
+            if (AxisAcceleration < 0) AxisAcceleration = 0;
+            if (TurnAcceleration < 0) TurnAcceleration = 0;
+        }
+
+        void Start()
+        {
+            Scramble();
+        }
+
+        public void Scramble()
+        {
+            axisDirectionPosition = Random.Range(0, 360);
+            axisDirectionSpeed = Random.Range(MinAxisDirectionSpeed, MaxAxisDirectionSpeed);
+            axisSpeed = Random.Range(MinAxisSpeed, MaxAxisSpeed);
+            turnSpeed = Random.Range(MinTurnSpeed, MaxTurnSpeed);
+            axis = Random.onUnitSphere;
+        }
+
+        void Update()
+        {
+            var dt = Time.deltaTime * Timescale;
+
+            axisDirectionSpeed += Random.Range(-1f, 1f) * dt * AxisDirectionAcceleration;
+            axisSpeed += Random.Range(-1f, 1f) * dt * AxisAcceleration;
+            turnSpeed += Random.Range(-1f, 1f) * dt * TurnAcceleration;
+
+            axisDirectionSpeed = Mathf.Clamp(axisDirectionSpeed, MinAxisDirectionSpeed, MaxAxisDirectionSpeed);
+            axisSpeed = Mathf.Clamp(axisSpeed, MinAxisSpeed, MaxAxisSpeed);
+            turnSpeed = Mathf.Clamp(turnSpeed, MinTurnSpeed, MaxTurnSpeed);
+
+            axisDirectionPosition += axisDirectionSpeed * dt;
+            axisDirectionPosition = axisDirectionPosition % 360;
+
+
+
+
+            var radp = Mathf.Deg2Rad * axisDirectionPosition;
+            var v2 = new Vector2(Mathf.Cos(radp), Mathf.Sin(radp));
+            v2 *= axisSpeed * dt;
+
+            axis = Quaternion.Euler(v2) * axis;
+
+            transform.rotation *= Quaternion.AngleAxis(turnSpeed * dt, Quaternion.Inverse(transform.rotation) * axis);
+
+            //transform.rotation = rot;
+            //transform.rotation *= Quaternion.AngleAxis(turnSpeed * dt, transform.InverseTransformDirection(Vector3.up));
+
+            //Quaternion q = Quaternion.AngleAxis()
+            //Quaternion q = Quaternion.AngleAxis(axisDirectionPosition, Vector3.up);
+            //axis *= Quaternion 
+            //axis *= Quaternion.AngleAxis(axisDirectionPosition, Vector3.forward)
+            //if(axisDirectionPosition < )
+        }
+    }
+}
---
+++ New Unity Project/Assets/LiqTech/Shader/ScreentonePBR.shader
@@ -0,0 +1,68 @@
+﻿Shader "Screentone/PBR" {
+	Properties {
+		_BaseTexture ("Base Texture", 2D) = "white" {}
+		_BaseTextureSize("Texture size", Int) = 32
+		_BaseRotationAngle("Base Texture Rotation", Range(0, 6.3)) = 0
+		_BasePrimaryColor ("Base Primary Color", Color) = (1,1,1,1)
+		_BaseSecondaryColor ("Base Secondary Color", Color) = (0,0,0,1)
+		_Glossiness ("Smoothness", Range(0,1)) = 0.5
+		_Metallic ("Metallic", Range(0,1)) = 0.0
+		
+
+		[Toggle(FULLCOLOR)]
+		_ToggleColor("Use full color", Float) = 0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+
+		#pragma shader_feature FULLCOLOR
+
+		#pragma target 3.0
+
+
+		struct Input {
+			float4 screenPos;
+		};
+
+		half _Glossiness;
+		half _Metallic;
+		
+		sampler2D _BaseTexture;
+		fixed4 _BasePrimaryColor;
+		fixed4 _BaseSecondaryColor;
+		float _BaseRotationAngle;
+		int  _BaseTextureSize;
+
+		inline float2 rotate(float2 pos, float angle){
+				float sina = sin(angle);
+				float cosa = cos(angle);
+				return float2(pos.x * cosa - pos.y * sina, pos.y * cosa + pos.x * sina);
+			}
+
+		//As the default shader, but using screenspace UVs
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			float2 screenUV = IN.screenPos.xy / IN.screenPos.w;
+			screenUV.x *= _ScreenParams.x / _ScreenParams.y;
+			screenUV *= _ScreenParams.y / _BaseTextureSize;
+			screenUV = rotate(screenUV, _BaseRotationAngle);
+			
+			#ifdef FULLCOLOR
+			o.Albedo = tex2D (_BaseTexture, screenUV) * _BasePrimaryColor;
+			#else
+			float c = tex2D (_BaseTexture, screenUV).a;
+			o.Albedo = lerp(_BaseSecondaryColor, _BasePrimaryColor, c).rgb;
+			#endif
+			o.Metallic = _Metallic;
+			o.Smoothness = _Glossiness;
+			o.Alpha = 1;
+			
+		}
+		ENDCG
+	} 
+	FallBack "Diffuse"
+	CustomEditor "ScreentonePBREditor"
+}
