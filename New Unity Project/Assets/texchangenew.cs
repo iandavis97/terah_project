2a281198a9bf3ba16f8c440c78bd0eef3f8ec12a
Orion Jakobovic <orionjak@gmail.com>
2018-11-06 13:25:38 -0500

    immediate change coroutines

--- New Unity Project/Assets/texchangenew.cs
+++ New Unity Project/Assets/texchangenew.cs
@@ -17,62 +17,115 @@ public Texture2D texture11;
 public Texture2D texture12;
 public Renderer  m_Renderer;
 public GameObject Calyx;
+    
 	// Use this for initialization
 	void Start () {
         m_Renderer = gameObject.GetComponent<Renderer> ();
-       StartCoroutine(change()); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+                   if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Joy"){
+                            
+                    StopCoroutine("changeanger");
+                    StopCoroutine("changesurprise");
+                    StopCoroutine("changedisgust");
+                    StopCoroutine("changesadness");
+                    StopCoroutine("changefear");
+
+                       StartCoroutine("changejoy"); 
+                
+            }
+	            else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Anger"){
+                            
+                    StopCoroutine("changejoy");
+                    StopCoroutine("changesurprise");
+                    StopCoroutine("changedisgust");
+                    StopCoroutine("changesadness");
+                    StopCoroutine("changefear");
+
+                   StartCoroutine("changeanger");
+                    
+            }
+              else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Surprise"){
+                            
+                      StopCoroutine("changejoy");
+                    StopCoroutine("changeanger");
+                    StopCoroutine("changedisgust");
+                    StopCoroutine("changesadness");
+                    StopCoroutine("changefear");
+
+                    StartCoroutine("changesurprise");
+                    
+            }
+              else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Disgust"){
+                            
+        StopCoroutine("changejoy");
+                    StopCoroutine("changeanger");
+                    StopCoroutine("changesurprise");
+                    StopCoroutine("changesadness");
+                    StopCoroutine("changefear");
+                     StartCoroutine("changedisgust");
+                    
+            }
+              else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Sadness"){
+                            
+                            StopCoroutine("changejoy");
+                    StopCoroutine("changeanger");
+                    StopCoroutine("changesurprise");
+                    StopCoroutine("changedisgust");
+                    StopCoroutine("changefear");
+                    StartCoroutine("changesadness");
+                    
+            }
+              else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Fear"){
+                            
+        StopCoroutine("changejoy");
+                    StopCoroutine("changeanger");
+                    StopCoroutine("changesurprise");
+                    StopCoroutine("changesadness");
+                    StopCoroutine("changedisgust");
+                    StartCoroutine("changefear");
+                    
+            }
+               
+            else{}
 	}
-    IEnumerator change(){
-        var counter=0;
-        while(counter>=0){
-            counter++;
-            if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Joy"){ 
-              yield return new WaitForSeconds(0.1f);
+        
+
+        IEnumerator changejoy(){
+         yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture1;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture2;  
-            }
-	            else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Anger"){ 
-                    counter++;
-              yield return new WaitForSeconds(0.1f);
+         m_Renderer.material.mainTexture = texture2; }
+    
+    IEnumerator changeanger(){
+    yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture3;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture4;  
-            }
-            else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Surprise"){ 
-              yield return new WaitForSeconds(0.1f);
+         m_Renderer.material.mainTexture = texture4; }
+    
+        IEnumerator changesurprise(){
+    yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture5;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture6;
-        counter++;  
-            }
-                        else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Disgust"){ 
-              yield return new WaitForSeconds(0.1f);
+         m_Renderer.material.mainTexture = texture6; }
+    
+        IEnumerator changedisgust(){
+    yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture7;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture8;
-        counter++;  
-            }
-                                    else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Sadness"){ 
-              yield return new WaitForSeconds(0.1f);
+         m_Renderer.material.mainTexture = texture8; }
+    
+        IEnumerator changesadness(){
+    yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture9;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture10;
-        counter++;  
-            }
-            else if(Calyx.GetComponent<EmotionalState>().GetCurrentEmotionName()=="Fear"){ 
-              yield return new WaitForSeconds(0.1f);
+         m_Renderer.material.mainTexture = texture10; }
+    
+        IEnumerator changefear(){
+    yield return new WaitForSeconds(0.1f);
         m_Renderer.material.mainTexture = texture11;
         yield return new WaitForSeconds(4.5f);
-         m_Renderer.material.mainTexture = texture12;
-        counter++;  
-            }
-            else{}
-        }
-    }
+         m_Renderer.material.mainTexture = texture12; }
 }
-
