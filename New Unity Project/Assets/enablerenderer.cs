aeb9daebfd037387db8f7f56e2128bcd4cbdb32d
Orion Jakobovic <orionjak@gmail.com>
2018-11-07 12:50:58 -0500

    renderer disable

---
+++ New Unity Project/Assets/enablerenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+
+public class enablerenderer : MonoBehaviour {
+
+	// Use this for initialization
+    void OnEnable(){
+        Lua.RegisterFunction("enablerender", this, typeof(enablerenderer).GetMethod("enablerender"));
+    Lua.RegisterFunction("disablerender", this, typeof(enablerenderer).GetMethod("disablerender"));	
+
+    }
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void enablerender(){
+         foreach (Renderer r in transform.GetComponentsInChildren<Renderer>(true))
+   r.enabled = true;
+
+    }
+       public void disablerender(){
+         foreach (Renderer r in transform.GetComponentsInChildren<Renderer>(true))
+   r.enabled = false;
+
+    }
+}
