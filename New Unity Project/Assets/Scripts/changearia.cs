2d862dd6a0663d7f2a032c0623f1762dbc01f725
aotihav <orionjak@gmail.com>
2019-05-23 11:20:21 -0400

    custom lua function to change aria

---
+++ New Unity Project/Assets/Scripts/changearia.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.LoveHate;
+using PixelCrushers.LoveHate;
+using PixelCrushers.LoveHate.ORKFrameworkSupport;
+public class changearia : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Lua.RegisterFunction("ChangeAria", this, typeof(changearia).GetMethod("ChangeAria"));
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeAria (string Emotion, string name){
+        gameObject = GameObject.Find(name);
+        switch (Emotion){
+            case "Joy":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, 200, 200, 200);
+                break;
+            case "Anger":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, 200, 200);
+                break;
+            case "Surprise":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, 200, 200, -200);
+                break;
+            case "Disgust":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, 200, -200);
+                break;
+            case "Sadness":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, -200, -200);
+                break;
+            case "Fear":
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, -200, 200);
+                break;
+        }
+
+    }
+}
