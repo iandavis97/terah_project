b140a6b1b65d47190dcad0f894ec0f1d914916e7
aotihav <orionjak@gmail.com>
2019-05-24 11:18:17 -0400

    setupcharbutton

---
+++ New Unity Project/Assets/Scripts/SetupChar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.LoveHate;
+using PixelCrushers.LoveHate.ORKFrameworkSupport;
+using ORKFramework.Behaviours;
+public class SetupChar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetupCharacter()
+    {
+        this.gameObject.AddComponent<Rigidbody>();
+        this.gameObject.AddComponent<changeariabutton>();
+        this.gameObject.AddComponent<OrkFactionMember>();
+        this.gameObject.AddComponent<EmotionalState>();
+        this.gameObject.AddComponent<Usable>();
+        this.gameObject.AddComponent<DialogueSystemTrigger>();
+        this.gameObject.AddComponent<serializeload>();
+        this.gameObject.AddComponent<deserializeload>();
+        this.gameObject.AddComponent<EventInteraction>();
+        this.gameObject.AddComponent<EventInteraction>();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/setupchareditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SetupChar))]
+public class setupchareditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SetupChar myScript = (SetupChar)target;
+        if (GUILayout.Button("Setup Character"))
+        {
+            myScript.SetupCharacter();
+        }
+
+    }
+}
+
+
