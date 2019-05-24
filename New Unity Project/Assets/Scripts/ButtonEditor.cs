7468a04c13d24c6c6a34291d8ca2ca7500f71d37
Orion Jakobovic <orionjak@gmail.com>
2019-05-23 22:27:59 -0400

    added aria change button

---
+++ New Unity Project/Assets/Scripts/ButtonEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(changeariabutton))]
+public class ButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        changeariabutton myScript = (changeariabutton)target;
+        if (GUILayout.Button("Joy"))
+        {
+            myScript.Joy();
+        }
+
+        if (GUILayout.Button("Anger"))
+        {
+            myScript.Anger();
+        }
+
+        if (GUILayout.Button("Surprise"))
+        {
+            myScript.Surprise();
+        }
+
+        if (GUILayout.Button("Disgust"))
+        {
+            myScript.Disgust();
+        }
+
+        if (GUILayout.Button("Sadness"))
+        {
+            myScript.Sadness();
+        }
+        if (GUILayout.Button("Fear"))
+        {
+            myScript.Fear();
+        }
+    }
+}
+
+
---
+++ New Unity Project/Assets/Scripts/changeariabutton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.LoveHate;
+using PixelCrushers.LoveHate;
+using PixelCrushers.LoveHate.ORKFrameworkSupport;
+public class changeariabutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Joy (){
+
+                gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, 200, 200, 200);
+
+    }
+    public void Anger()
+    {
+
+        this.gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, 200, 200);
+    }
+    public void Surprise()
+    {
+
+        this.gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, 200, 200, -200);
+
+    }
+    public void Disgust()
+    {
+
+        this.gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, 200, -200);
+
+    }
+    public void Sadness()
+    {
+
+        this.gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, -200, -200);
+
+    }
+
+
+    public void Fear()
+    {
+
+        this.gameObject.GetComponent<OrkFactionMember>().pad.Modify(0, -200, -200, 200);
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
