bba601dcba1705030a0e6ba893696a07d74ca813
Orion Jakobovic <orionjak@gmail.com>
2019-05-30 09:26:21 -0400

    changing sfx based on char (needs work maybe?)

---
+++ New Unity Project/Assets/Scripts/changeSfx.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+public class changeSfx : MonoBehaviour
+{
+
+    public void OnBeginTypewriter() // Assign to typewriter's OnBegin() event.
+    {
+        // Look up character's audio clip:
+        var actorName = DialogueManager.currentConversationState.subtitle.listenerInfo.nameInDatabase;
+        var clipName = DialogueManager.masterDatabase.GetActor(actorName).LookupValue("AudioClip");
+        var clip = Resources.Load<AudioClip>(clipName);
+
+        // Assign to typewriter:
+        var typewriter = GetComponent<AbstractTypewriterEffect>();
+        typewriter.audioClip = clip;
+  }
+}
