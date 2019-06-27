937bcfc1bb2ae6eff2f4a8212c56db5036542dde
Ian Davis <iandavis97@gmail.com>
2019-06-27 16:09:51 -0400

    misc. bug fixes

--- New Unity Project/Assets/Scripts/lockplayer.cs
+++ New Unity Project/Assets/Scripts/lockplayer.cs
@@ -5,22 +5,24 @@ using ORKFramework;
 using ORKFramework.Behaviours;
 using Invector.vCharacterController;
 
-public class lockplayer : MonoBehaviour {
+public class lockplayer : MonoBehaviour
+{
         private vThirdPersonInput m_input = null;	// Use this for initialization
-	void Start () {
-            m_input = GetComponent<vThirdPersonInput>();	}
+	void Start ()
+    {
+            m_input = GetComponent<vThirdPersonInput>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-        if (ORK.Control.InEvent || ORK.Control.InMenu || ORK.Control.Blocked)
+	void Update ()
+    {
+        if (ORK.Control.InEvent || ORK.Control.InMenu || ORK.Control.Blocked )
         {
             m_input.lockInput = true;
-            ORK.InputKeys.Get(9).Blocked = true;//locking Tab key
         }
         else
         {
             m_input.lockInput = false;
-            ORK.InputKeys.Get(9).Blocked = false;//unlocking Tab key
         }  
 	}
 }
